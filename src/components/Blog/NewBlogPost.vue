93cb3d3a6853a71ed81699235c7bfc60da4bc472
Alibh <alibh619@gmail.com>
2018-03-08 18:05:55 +0530

    Blog Component Updated with FireBase integration and File Upload

--- src/components/Blog/NewBlogPost.vue
+++ src/components/Blog/NewBlogPost.vue
@@ -3,18 +3,20 @@
     <div class="ui form">
       <div class="field">
         <label>Title</label>
-        <input type="text" placeholder="Enter Title">
+        <input type="text" placeholder="Enter Title" v-model="blogPost.title">
       </div>
       <div class="field">
         <label>Summery</label>
-        <textarea placeholder="Enter Summery" maxlength="30"></textarea>
+        <textarea placeholder="Enter Summery" maxlength="30" v-model="blogPost.hook"></textarea>
       </div>
     </div>
     <br>
-    <app-blog-content :content="item" v-for="item in blogPost.content"></app-blog-content>
-    <button class="ui right floated primary button">Submit</button>
-    <button class="ui right floated teal button">Add More Content</button>
-    <button class="ui right floated button">Cancel</button>
+    <div v-for="(item,index) in blogPost.content" :key="index">
+      <app-blog-content :content="item" :upload="upload"></app-blog-content>
+    </div>
+    <button class="ui right floated primary button" @click="submit">Submit</button>
+    <button class="ui right floated teal button" @click="onAddMoreContent">Add More Content</button>
+    <button class="ui right floated button" @click="close">Cancel</button>
     <br>
     <br>
   </div>
@@ -25,9 +27,15 @@
 
   export default {
     name: "blog-detail",
-    props: ['close', 'blogPost'],
+    props: ['close', 'blogPost', 'newContent', 'upload', 'submit'],
     components: {
       appBlogContent
+    },
+    methods: {
+      onAddMoreContent() {
+        this.newContent();
+        this.$forceUpdate();
+      }
     }
   }
 </script>
