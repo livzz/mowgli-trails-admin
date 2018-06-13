d0177254b9bb19fa705c20824b84e798a64a7b5a
Alibh Deori <alibh619@gmail.com>
2018-06-13 12:03:34 +0530

    Refactored

--- src/components/Admin/AdminMain.vue
+++ src/components/Admin/AdminMain.vue
@@ -22,40 +22,40 @@
 </template>
 
 <script>
-  import {db} from '../../firebase';
-  import appCraft from './DetailCraft';
+import { db } from '../../firebase';
+import appCraft from './DetailCraft';
 
-  export default {
-    data() {
-      return {
-        loading: true,
-        craft: null,
-        raised: false,
-        hoveredCraft: -1
-      }
-    },
-    components: {
-      appCraft
-    },
-    firebase: {
-      craftCustomers: {
-        source: db.ref('craftCustomers'),
-        // this is called once the data has been retrieved from firebase
-        readyCallback() {
-          this.loading = false
-        }
-      }
-    },
-    methods: {
-      viewDetail(craft) {
-        this.craft = craft;
-        console.log(craft);
+export default {
+  data() {
+    return {
+      loading: true,
+      craft: null,
+      raised: false,
+      hoveredCraft: -1,
+    };
+  },
+  components: {
+    appCraft,
+  },
+  firebase: {
+    craftCustomers: {
+      source: db.ref('craftCustomers'),
+      // this is called once the data has been retrieved from firebase
+      readyCallback() {
+        this.loading = false;
       },
-      closeDetail() {
-        this.craft = null;
-      }
-    }
-  };
+    },
+  },
+  methods: {
+    viewDetail(craft) {
+      this.craft = craft;
+      console.log(craft);
+    },
+    closeDetail() {
+      this.craft = null;
+    },
+  },
+};
 </script>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import SuiVue from 'semantic-ui-vue';
-import vueFire from 'vuefire'
+import vueFire from 'vuefire';
 
 import App from './App';
 import router from './router';
